3a82df900dd156d479b70359f21614c096ec8efd
DENIT <denitjoseph25@gmail.com>
2026-09-04 02:04:21 +0530

    Add Dockerfile, .dockerignore, and updated container dependencies

--- app/services/llm_service.py
+++ app/services/llm_service.py
@@ -1,3 +1,5 @@
+import os
+
 from langchain_ollama import ChatOllama
 
 
@@ -5,5 +7,9 @@ def get_llm():
 
     return ChatOllama(
         model="gemma3:4b",
-        temperature=0
+        temperature=0,
+        base_url=os.getenv(
+            "OLLAMA_BASE_URL",
+            "http://localhost:11434"
+        )
     )
